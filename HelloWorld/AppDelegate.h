767918bd9e96997848453d1d7e796e1135b45b6e
Tackgyu Song <rusiper09@naver.com>
2014-08-04 03:51:55 +0900

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Song Tack-gyu on 2014. 8. 4..
+//  Copyright (c) 2014년 rusiper09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Song Tack-gyu on 2014. 8. 4..
+//  Copyright (c) 2014년 rusiper09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
